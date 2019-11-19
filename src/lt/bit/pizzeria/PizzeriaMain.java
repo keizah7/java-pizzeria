b3dc5bf8f2bb0099d858b56f242584df037c4a17
keizah7 <murashka.arturas@gmail.com>
2019-11-19 21:04:01 +0200

    Code refactor

--- src/lt/bit/pizzeria/PizzeriaMain.java
+++ src/lt/bit/pizzeria/PizzeriaMain.java
@@ -1,110 +1,120 @@
 package lt.bit.pizzeria;
 
-import lt.bit.pizzeria.items.*;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
-import java.util.regex.Pattern;
 
 public class PizzeriaMain {
-    private static final String PATH = "/home/keizah/java/java-pizzeria/txt/menu.txt";
-    private static boolean space = true;
     private static boolean ordering = true;
     private static Map<String, List> menuItems = new TreeMap<>(Collections.reverseOrder());
-    private static List<MenuItem> items = new LinkedList<>();
     private static List<MenuItem> allItems = new LinkedList<>();
     private static Map<MenuItem, Integer> ordered = new TreeMap<>();
-    private static String category = "";
     private static int i = 0;
     private static double price = 0;
     private static Scanner c = new Scanner(System.in);
+    private static String category = "";
 
     public static void main(String[] args) {
-        createItemsFromMenu();
         welcome();
     }
 
     private static void welcome() {
-        System.out.println("Sveiki atvykę į Mūsų restoraną, prašome, sėstis čia.\nAr atnešti Jums meniu? (t / n)");
-
+        Message.println("welcome");
         boolean welcome = true;
+
+        label:
         do {
             String answer = c.nextLine();
 
-            if (answer.equals("t")) {
-                printMenu();
-
-                review();
-                break;
-            } else if (answer.equals("n")) welcome = false;
-            else System.out.println("t(taip) arba n(ne)");
+            switch (answer) {
+                case "taip":
+                    menuItems = new CreateMenuItems().get();
+                    printMenu();
+                    review();
+                    break label;
+                case "ne":
+                    welcome = false;
+                    break;
+                default:
+                    Message.println("welcome2");
+                    break;
+            }
         } while (welcome);
 
-        System.out.println("\n\tViso gero!");
+        Message.print("bye");
     }
 
     private static void review() {
-        System.out.println("Peržiūrėkite meniu. (i(išeiti) / u(užsisakyti)");
+        Message.println("review");
 
         boolean review = true;
+        label:
         do {
             String answer = c.next();
-            if (answer.equals("u")) {
-                ordering();
-                break;
-            } else if (answer.equals("i")) review = false;
-            else System.out.println("(i(išeiti) / u(užsisakyti)");
+            switch (answer) {
+                case "uzsakyti":
+                    ordering();
+                    break label;
+                case "iseiti":
+                    review = false;
+                    break;
+                default:
+                    Message.println("review2");
+                    break;
+            }
         } while (review);
     }
 
     private static void ordering() {
-        String text = "Rinkitės: (int / int ...int) / (.(išsirinkau) / i(išeiti)) / m(pažiūrėti į meniu) ";
-        System.out.print(text);
-        int lastInt = 0;
-        ordered = new TreeMap<>();
+        Message.println("choose");
+        MenuItem lastItem = variablesInit();
 
         do {
             String answer = c.next();
 
-            if (answer.equals("i")) break;
-            else if (answer.equals("m")) {
+            if (answer.equals("iseiti")) break;
+            else if (answer.equals("meniu")) {
                 printMenu();
 
-                System.out.print(text);
-            } else if (answer.equals(".")) {
+                Message.println("choose");
+            } else if (answer.equals("issirinkau")) {
                 finishOrdering(false);
                 break;
             } else if (answer.matches("\\d+")) {
                 int answerInt = Integer.parseInt(answer);
-                if (answerInt <= allItems.size() & answerInt > 0) {
-                    int uzs = answerInt - 1;
 
-                    MenuItem item = allItems.get(uzs);
+                if (answerInt <= allItems.size() & answerInt > 0) {
+                    int booked = answerInt - 1;
+
+                    MenuItem item = allItems.get(booked);
 
                     System.out.print(Color.BBLACK);
-                    if (ordered.containsKey(item)) {
+                    if ((boolean) ordered.containsKey(item)) {
                         Integer count = ordered.get(item);
-                        ordered.put(item, ++count);
-                        if (lastInt == uzs) System.out.print("->" + count + "vnt. ");
-                        else System.out.print(item.getType() + " (" + count + "vnt.), ");
+
+                        ordered.put(item, count);
+
+                        if (lastItem == item) System.out.print("->" + count + "vnt. ");
+                        else if (count > 1) System.out.print(item.getType() + " (" + count + "vnt.), ");
                     } else {
                         System.out.print(item.getType() + ", ");
-                        if (ordered.size() % 5 == 0) System.out.println();
                         ordered.put(item, 1);
+                        if (ordered.size() % 4 == 0) System.out.println();
                     }
                     System.out.print(Color.RESET);
+                    lastItem = item;
                 } else
                     System.out.print("\n" + Color.RED + answerInt + " - produkto meniu sąraše nėra!" + Color.BBLACK + " ");
-                lastInt = answerInt - 1;
-            } else {
-                System.out.println("int(enter) / (.(išsirinkau) / i(išeiti)) / m(pažiūrėti į meniu)");
-                ordering = true;
-            }
+            } else Message.println("choose2");
         } while (ordering);
+    }
+
+    private static MenuItem variablesInit() {
+        MenuItem lastItem = null;
+        ordered = new TreeMap<>();
+        category = "";
+        i = 0;
+        return lastItem;
     }
 
     private static void finishOrdering(boolean finished) {
@@ -117,8 +127,6 @@ public class PizzeriaMain {
                         + "\n\n" + Color.RESET
         );
         price = 0;
-
-        System.out.println();
 
         ordered.forEach((item, count) ->
         {
@@ -142,46 +150,35 @@ public class PizzeriaMain {
     }
 
     private static void finalDecision() {
-        String text = Color.UBLACK + "\nRinkitės:" + Color.RESET + " k(" + Color.GREEN + "PATVIRTINU" + Color.RESET + ") / u(" + Color.BLUE + "Užsisakyti iš naujo" + Color.RESET + ") / i(" + Color.RED + "išeiti" + Color.RESET + ") ";
-        System.out.print(text);
-
+        Message.println("finalChoose");
         boolean decision = true;
+
         label:
         do {
             String answer = c.next();
             switch (answer) {
-                case "k":
+                case "testi":
                     finishOrdering(true);
                     break label;
-                case "i":
+                case "iseiti":
                     decision = false;
                     break;
-                case "u":
-                    System.out.println("\n\n\tRinkis iš naujo.........");
+                case "persigalvojau":
+                    Message.println("finalChoose");
+                    System.out.println("\n\n\tRinkitės iš naujo..");
                     ordering();
                     break label;
                 default:
-                    System.out.println(text);
+                    Message.println("finalChoose");
                     break;
             }
         } while (decision);
     }
 
     private static void printMenu() {
-        printMenuHeader();
+        Message.print("header");
         menuItems.forEach(PizzeriaMain::printItems);
-        printMenuFooter();
-    }
-
-    private static void printMenuFooter() {
-        System.out.println("\n" + Color.RED_BG + "" + Color.BBLACK + "\n\t" + "\u00a9 Artūras M.\n");
-        System.out.println(Color.RESET);
-    }
-
-    private static void printMenuHeader() {
-        category = "";
-        i = 0;
-        System.out.print(Color.RED_BG + "" + Color.BBLACK + "\n\tSaulėtekio Pizzeria & Wok\n\t" + Color.WHITE + "Restorano meniu:\n\n" + Color.RESET);
+        Message.print("footer");
     }
 
     private static void printItem(String key, MenuItem item) {
@@ -193,19 +190,6 @@ public class PizzeriaMain {
         printItemInfo(item);
     }
 
-    private static void printItemInfo(MenuItem item) {
-        if (item.getCapacity() != null)
-            if (!item.getCapacity().endsWith(")")) System.out.print(item.getCapacity() + " / ");
-        System.out.println(String.format("%4.2f €", item.getPrice()));
-        if (item.getInfo() != null) if (item.getInfo().endsWith(")")) System.out.println("\t\t" + item.getInfo());
-    }
-
-    private static void printItemName(MenuItem item) {
-        System.out.println(Color.BLACK_BG);
-        System.out.print("\t" + Color.RED + (++i < 10 ? " " + i : i) + ". " + Color.WHITE);
-        System.out.print(item.getType() + " ");
-    }
-
     private static void printItemTitle(String key) {
         if (!category.equals(key)) {
             if (i != 0) System.out.println();
@@ -215,67 +199,22 @@ public class PizzeriaMain {
         }
     }
 
+    private static void printItemInfo(MenuItem item) {
+        if (item.getCapacity() != null)
+            if (!item.getCapacity().endsWith(")")) System.out.print(item.getCapacity() + " / ");
+        System.out.println(String.format("%4.2f €", item.getPrice()));
+        if (item.getInfo() != null) if (item.getInfo().endsWith(")")) System.out.println("\t\t" + item.getInfo());
+    }
+
+    private static void printItemName(MenuItem item) {
+        System.out.print(Color.BLACK_BG+"\n\t" + Color.RED + (++i < 10 ? " " + i : i) + ". " + Color.WHITE);
+        System.out.print(item.getType() + " ");
+    }
+
     private static void printDots(MenuItem s) {
         final int dots = 200;
 
         System.out.print(".".repeat(dots - s.getType().length() - ((s.getCapacity() != null) ? (!s.getCapacity().endsWith(")") ? s.getCapacity().length() + 3 : 0) : 0)) + " ");
-    }
-
-    private static void createItemsFromMenu() {
-        try {
-            Files.readAllLines(Paths.get(PATH))
-                    .stream()
-                    .map(s -> s.split("\n"))
-                    .forEach(
-                            strings -> {
-                                String[] arr = strings[0].split(Pattern.quote("|"));
-                                if (!category.equals(arr[0])) {
-                                    category = arr[0];
-                                    items = new LinkedList<>();
-                                }
-                                menuItems.put(category, addMenuItemsToList(arr));
-                            }
-                    );
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static List<MenuItem> addMenuItemsToList(String[] arr) {
-        String category = arr[0];
-        items.add(createMenuItemByCategoryName(arr));
-        Collections.sort(menuItems.getOrDefault(category, new LinkedList<>()));
-
-        return items;
-    }
-
-    private static MenuItem createMenuItemByCategoryName(String[] arr) {
-        String item = arr[0];
-        double p = Double.parseDouble(arr[1]);
-        String t = arr[2];
-        String info = arr.length > 3 ? arr[3] : "";
-        String capacity = arr.length > 4 ? arr[4] : "";
-
-        return returnMenuItem(item, p, t, info, capacity);
-    }
-
-    private static MenuItem returnMenuItem(String item, double p, String t, String info, String capacity) {
-        switch (item) {
-            case "Wok":
-                return new Wok(t, p, info);
-            case "Pica":
-                return new Pizza(t, p, info);
-            case "Alus":
-                return new Beer(t, p, info, capacity);
-            case "Gaivusis gėrimas":
-                return new Beverage(t, p, info, capacity);
-            case "Arbata":
-                return new Tea(t, p);
-            case "Kava":
-                return new Coffee(t, p);
-            default:
-                return null;
-        }
     }
 
     private static void printItems(String key, List value) {
